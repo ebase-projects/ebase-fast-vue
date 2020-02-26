69b919a5a9a428ebe74c4312b599dcd04f98d4a4
liudw <ldw4033@gmail.com>
2020-02-26 16:48:06 +0800

    feat：增加用户模块左侧部门树

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,10 +1,80 @@
 <template>
-  <div />
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :span="4" :xs="24">
+        <!--部门数据-->
+        <div class="head-container">
+          <el-input
+            v-model="deptName"
+            placeholder="请输入部门名称"
+            clearable
+            size="small"
+            prefix-icon="el-icon-search"
+            style="margin-bottom: 20px"
+          />
+        </div>
+        <div class="head-container">
+          <el-tree
+            ref="tree"
+            :data="deptOptions"
+            :props="defaultProps"
+            :expand-on-click-node="false"
+            :filter-node-method="filterNode"
+            default-expand-all
+            @node-click="handleNodeClick"
+          />
+        </div>
+      </el-col>
+      <el-col :span="20" :xs="24">
+        <!--用户数据-->
+      </el-col>
+    </el-row>
+
+  </div>
 </template>
 
 <script>
+import { listDept } from '@/api/system/dept'
+// import Treeselect from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 export default {
-  name: 'Index'
+  name: 'User',
+  // components: { Treeselect },
+  data() {
+    return {
+      deptName: undefined,
+      deptOptions: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  watch: {
+    // 根据名称筛选部门树
+    deptName(val) {
+      this.$refs.tree.filter(val)
+    }
+  },
+  created() {
+    this.getTreeselect()
+  },
+  methods: {
+    // 获取部门树列表
+    getTreeselect: function() {
+      listDept().then(response => {
+        this.deptOptions = response.data
+      })
+    },
+    // 筛选节点
+    filterNode(value, data) {
+      if (!value) return true
+      return data.name.indexOf(value) !== -1
+    },
+    handleNodeClick: function() {
+
+    }
+  }
 }
 </script>
 

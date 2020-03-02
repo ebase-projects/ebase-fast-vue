7e9638daa3fef9e7686ae684f347a368598ce64b
liudw <ldw4033@gmail.com>
2020-03-02 22:07:59 +0800

    feat:异常处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -75,7 +75,7 @@ service.interceptors.response.use(
   error => {
     console.log('err' + error) // for debug
     Message({
-      message: error.response.data.content || error.response.data.msg,
+      message: error.response.data.content || error.response.data.msg || '服务器异常，请联系管理员',
       type: 'error',
       duration: 5 * 1000
     })

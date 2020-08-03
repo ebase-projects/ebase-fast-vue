28034a977650547d4a5d6f1ec7e05581089311a1
sy01121 <sy01121@163.com>
2020-08-03 16:27:13 +0800

    表格分页样式修改

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
     },
     proxy: {
       [process.env.VUE_APP_BASE_API]: {
-        target: 'http://localhost:8080/',
+        target: 'http://miniapp.nps.dwliu.me',
         // target: 'http://ebase.nps.dwliu.me',
         changeOrigin: true, // 是否跨域
         // secure: true,
